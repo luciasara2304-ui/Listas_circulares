cf2e464947214cd1a1d719fd780b5866a8a630f9
USERVT <luciasara2304@gmail.com>
2026-09-04 17:26:29 -0500

    Terminado

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,40 @@ public class Main {
         lista.add("Circular 5");
         System.out.println("Nodo encontrado: " + lista.get(lista.search("Circular 3")));
 
+        System.out.println("¿Insertado por nodo?: " + lista.insert(n2, "Circular Insertado Nodo"));
+        System.out.println("¿Insertado por objeto?: " + lista.insert("Circular 5", "Circular Insertado Objeto"));
+
+        lista.insertHead("Circular Inicio");
+        lista.insertTail("Circular Final");
+
+        // CORREGIDO: Usamos ((Node)...).dato
+        System.out.println("Nueva Cabeza: " + ((Node) lista.getHead()).dato);
+        System.out.println("Nueva Cola: " + ((Node) lista.getTail()).dato);
+
+        System.out.println("Dato original n2: " + lista.get(n2));
+
+        // CORREGIDO: Modificamos n2 indicando el nodo n2 y el nuevo texto "Modificado Circular"
+        System.out.println("¿Se modificó n2?: " + lista.set(n2, "Modificado Circular"));
+        System.out.println("Nuevo dato de n2: " + lista.get(n2));
+
+        // CORREGIDO: Al estar sano n2, ahora remove(n2) dará true
+        System.out.println("¿Se eliminó n2?: " + lista.remove(n2));
+        System.out.println("¿Contiene 'Inexistente'?: " + lista.contains("Inexistente"));
+
+        // --- Operaciones de expansión ---
+        System.out.println("Arreglo: " + Arrays.toString(lista.toArray()));
+
+        Node desde = lista.search("Circular Inicio");
+        Node hasta = lista.search("Circular 3");
+        MiListaCircular sublista = lista.subList(desde, hasta);
+        System.out.println("Sublista: " + Arrays.toString(sublista.toArray()));
+
+        Object[] miArreglo = new Object[lista.getSize()];
+        lista.toArray(miArreglo);
+        System.out.println("Arreglo (con parámetro): " + Arrays.toString(miArreglo));
+
+        MiListaCircular listaOrdenada = lista.sortList();
+        System.out.println("Lista Ordenada: " + Arrays.toString(listaOrdenada.toArray()));
 
         lista.clear();
         System.out.println("DESPUES DE VACIAR LA LISTA ");
--- src/MiListaCircular.java
+++ src/MiListaCircular.java
@@ -10,32 +10,37 @@ public class MiListaCircular implements ListInterface {
         this.size = 0;
     }
 
-    /*
-        essential operations
-        */
-    public boolean isEmpty(){
+    // --- ESSENTIAL OPERATIONS ---
+
+    @Override
+    public boolean isEmpty() {
         return this.size == 0;
     }
 
-    public int getSize(){
+    @Override
+    public int getSize() {
         return this.size;
     }
 
-    public void clear(){
+    @Override
+    public void clear() {
         this.cabeza = null;
         this.cola = null;
         this.size = 0;
     }
 
-    public Object getHead(){
+    @Override
+    public Object getHead() {
         return this.cabeza;
     }
 
-    public Object getTail(){
+    @Override
+    public Object getTail() {
         return this.cola;
     }
 
-    public Object get(Node node){
+    @Override
+    public Object get(Node node) {
         if (isEmpty() || node == null) return null;
 
         Node actual = this.cabeza;
@@ -53,7 +58,8 @@ public class MiListaCircular implements ListInterface {
         return null;
     }
 
-    public Node search(Object object){
+    @Override
+    public Node search(Object object) {
         if (isEmpty() || object == null) return null;
 
         Node actual = this.cabeza;
@@ -71,30 +77,249 @@ public class MiListaCircular implements ListInterface {
         return null;
     }
 
-    public boolean add(Object object){}
+    @Override
+    public boolean add(Object object) {
+        return insertTail(object);
+    }
 
-    public boolean insert(Node node, Object object){}
+    @Override
+    public boolean insert(Node node, Object object) {
+        if (isEmpty() || node == null) return false;
 
-    public boolean insert(Object objectRef, Object object){}
+        Node actual = this.cabeza;
+        do {
+            if (actual == node || actual.dato == node) {
+                Node nuevo = new Node(object);
+                nuevo.siguiente = actual.siguiente;
+                actual.siguiente = nuevo;
 
-    public boolean insertHead(Object object){}
+                if (actual == this.cola) {
+                    this.cola = nuevo;
+                }
+                this.size++;
+                return true;
+            }
+            actual = actual.siguiente;
+        } while (actual != this.cabeza);
 
-    public boolean insertTail(Object object){}
+        return false;
+    }
 
-    public boolean set(Node node, Object object){}
+    @Override
+    public boolean insert(Object objectRef, Object object) {
+        Node nodoEncontrado = search(objectRef);
+        if (nodoEncontrado != null) {
+            return insert(nodoEncontrado, object);
+        }
+        return false;
+    }
 
-    public boolean remove(Node node){}
+    @Override
+    public boolean insertHead(Object object) {
+        Node nuevo = new Node(object);
+        if (isEmpty()) {
+            this.cabeza = nuevo;
+            this.cola = nuevo;
+            this.cola.siguiente = this.cabeza;
+        } else {
+            nuevo.siguiente = this.cabeza;
+            this.cabeza = nuevo;
+            this.cola.siguiente = this.cabeza;
+        }
+        this.size++;
+        return true;
+    }
 
-    /*
-    expansion operations
-     */
-    public boolean contains(Object object){}
+    @Override
+    public boolean insertTail(Object object) {
+        Node nuevo = new Node(object);
+        if (isEmpty()) {
+            this.cabeza = nuevo;
+            this.cola = nuevo;
+            this.cola.siguiente = this.cabeza;
+        } else {
+            this.cola.siguiente = nuevo;
+            this.cola = nuevo;
+            this.cola.siguiente = this.cabeza;
+        }
+        this.size++;
+        return true;
+    }
 
-    public Object[] toArray(){}
+    @Override
+    public boolean set(Node node, Object object) {
+        if (isEmpty() || node == null) return false;
 
-    public Object[] toArray(Object[] object){}
+        Node actual = this.cabeza;
+        do {
+            if (actual == node || actual.dato == node) {
+                actual.dato = object;
+                return true;
+            }
+            actual = actual.siguiente;
+        } while (actual != this.cabeza);
 
-    public MiListaCircular subList(Node from, Node to){}
+        return false;
+    }
 
-    public MiListaCircular sortList(){}
+    @Override
+    public boolean remove(Node node) {
+        if (isEmpty() || node == null) return false;
+
+        Node actual = this.cabeza;
+        Node anterior = this.cola;
+
+        do {
+            if (actual == node || actual.dato == node) {
+                if (this.size == 1) {
+                    this.cabeza = null;
+                    this.cola = null;
+                } else {
+                    anterior.siguiente = actual.siguiente;
+
+                    if (actual == this.cabeza) {
+                        this.cabeza = actual.siguiente;
+                        this.cola.siguiente = this.cabeza;
+                    }
+                    if (actual == this.cola) {
+                        this.cola = anterior;
+                        this.cola.siguiente = this.cabeza;
+                    }
+                }
+                this.size--;
+                return true;
+            }
+            anterior = actual;
+            actual = actual.siguiente;
+        } while (actual != this.cabeza);
+
+        return false;
+    }
+
+    // --- EXPANSION OPERATIONS ---
+
+    @Override
+    public boolean contains(Object object) {
+        return search(object) != null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] arreglo = new Object[this.size];
+        if (isEmpty()) return arreglo;
+
+        Node actual = this.cabeza;
+        int i = 0;
+        do {
+            Object valor = actual.dato;
+            while (valor instanceof Node) {
+                valor = ((Node) valor).dato;
+            }
+            arreglo[i++] = valor;
+            actual = actual.siguiente;
+        } while (actual != this.cabeza && i < this.size);
+
+        return arreglo;
+    }
+
+    @Override
+    public Object[] toArray(Object[] object) {
+        if (object == null || object.length < this.size) {
+            object = new Object[this.size];
+        }
+
+        if (!isEmpty()) {
+            Node actual = this.cabeza;
+            int i = 0;
+            do {
+                Object valor = actual.dato;
+                while (valor instanceof Node) {
+                    valor = ((Node) valor).dato;
+                }
+                object[i++] = valor;
+                actual = actual.siguiente;
+            } while (actual != this.cabeza && i < this.size);
+        }
+
+        if (object.length > this.size) {
+            object[this.size] = null;
+        }
+
+        return object;
+    }
+
+    @Override
+    public MiListaCircular subList(Node from, Node to) {
+        MiListaCircular sublista = new MiListaCircular();
+        if (from == null || to == null || isEmpty()) return sublista;
+
+        Node actual = this.cabeza;
+        Node inicio = null;
+
+        do {
+            if (actual == from || actual.dato == from) {
+                inicio = actual;
+                break;
+            }
+            actual = actual.siguiente;
+        } while (actual != this.cabeza);
+
+        if (inicio == null) return sublista;
+
+        actual = inicio;
+        do {
+            Object valor = actual.dato;
+            while (valor instanceof Node) {
+                valor = ((Node) valor).dato;
+            }
+            sublista.add(valor);
+
+            if (actual == to || actual.dato == to) {
+                break;
+            }
+            actual = actual.siguiente;
+        } while (actual != this.cabeza);
+
+        return sublista;
+    }
+
+    @Override
+    public MiListaCircular sortList() {
+        MiListaCircular listaOrdenada = new MiListaCircular();
+        if (isEmpty()) return listaOrdenada;
+
+        // 1. Clonar elementos planos a la nueva lista
+        Node actual = this.cabeza;
+        do {
+            Object valor = actual.dato;
+            while (valor instanceof Node) {
+                valor = ((Node) valor).dato;
+            }
+            listaOrdenada.add(valor);
+            actual = actual.siguiente;
+        } while (actual != this.cabeza);
+
+        // 2. Bubble Sort manual sobre la lista circular
+        boolean huboIntercambio;
+        do {
+            huboIntercambio = false;
+            Node p1 = listaOrdenada.cabeza;
+
+            do {
+                String val1 = p1.dato.toString();
+                String val2 = p1.siguiente.dato.toString();
+
+                if (p1.siguiente != listaOrdenada.cabeza && val1.compareTo(val2) > 0) {
+                    Object temp = p1.dato;
+                    p1.dato = p1.siguiente.dato;
+                    p1.siguiente.dato = temp;
+                    huboIntercambio = true;
+                }
+                p1 = p1.siguiente;
+            } while (p1.siguiente != listaOrdenada.cabeza);
+
+        } while (huboIntercambio);
+
+        return listaOrdenada;
+    }
 }
